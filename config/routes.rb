82f18981e91cf81780a36308716fa78d224781c0
Jayanti Pesa <jayanti.pesa@gmail.com>
2020-09-10 14:01:38 +0530

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # application is the controller name, #hello is the method present in application controller to which request will be sent to
 
   root 'pages#home'
+  get 'about', to: 'pages#about'
 
 end
