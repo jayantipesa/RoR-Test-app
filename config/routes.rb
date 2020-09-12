05bd1ca755b16388b0ee68696fd1a6a0ec84ddd2
Jayanti Pesa <jayanti.pesa@gmail.com>
2020-09-13 01:24:18 +0530

    General changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   # application is the controller name, #hello is the method present in application controller to which request will be sent to
 
   root 'pages#home'
